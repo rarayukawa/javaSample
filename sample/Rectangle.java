012aadd138c1ba4727bae5db67954eb87103b0be
rarayukawa <r1u2u18@gmail.com>
2021-08-15 22:28:19 +0900

    constructor

--- sample/Rectangle.java
+++ sample/Rectangle.java
@@ -1,5 +1,32 @@
 package sample;
 
 public class Rectangle {
-
+	static int counter = 0;
+	int width;
+	int height;
+	Rectangle(int w, int h) {
+		setSize(w, h);
+		counter++;
+	}
+	
+	Rectangle() {
+		setSize(10, 20);
+		counter++;
+	}
+	
+	void setSize(int w, int h) {
+		this.width = w;
+		this.height = h;
+	}
+	
+	int getArea() {
+		return width * height;
+	}
+	
+	public static void main(String[] args) {
+		Rectangle r1 = new Rectangle();
+		System.out.println("r1.width = " + r1.width + "\nr1.height = " + r1.height + "\nr1.getArea() = " + r1.getArea() + "\nコンストラクタの数" + counter);
+		Rectangle r2 = new Rectangle(123, 45);
+		System.out.println("r2.width = " + r2.width + "\nr2.height = " + r2.height + "\nr2.getArea() = " + r2.getArea() + "\nコンストラクタの数" + counter);
+	}
 }
