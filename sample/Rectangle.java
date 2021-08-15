c6b9804d09e7f0bda380e1961554eec139d78097
rarayukawa <r1u2u18@gmail.com>
2021-08-15 23:01:33 +0900

    field

--- sample/Rectangle.java
+++ sample/Rectangle.java
@@ -4,6 +4,9 @@ public class Rectangle {
 	static int counter = 0;
 	int width;
 	int height;
+	static int getCounter() {
+		return counter;
+	}
 	Rectangle(int w, int h) {
 		setSize(w, h);
 		counter++;
@@ -25,8 +28,12 @@ public class Rectangle {
 	
 	public static void main(String[] args) {
 		Rectangle r1 = new Rectangle();
-		System.out.println("r1.width = " + r1.width + "\nr1.height = " + r1.height + "\nr1.getArea() = " + r1.getArea() + "\nコンストラクタの数" + counter);
+		System.out.println("r1.width = " + r1.width + "\nr1.height = " + r1.height + "\nr1.getArea() = " + r1.getArea() + "\nコンストラクタの数" + counter + "\n" + r1);
 		Rectangle r2 = new Rectangle(123, 45);
-		System.out.println("r2.width = " + r2.width + "\nr2.height = " + r2.height + "\nr2.getArea() = " + r2.getArea() + "\nコンストラクタの数" + counter);
+		System.out.println("r2.width = " + r2.width + "\nr2.height = " + r2.height + "\nr2.getArea() = " + r2.getArea() + "\nコンストラクタの数" + counter + "\n" + r2);
+	}
+	@Override
+	public String toString() {
+		return "Rectangle(" + width + ", " + height + ")";
 	}
 }
