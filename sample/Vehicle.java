2dff1b872e7d8ecd98928ec881ff0d4a590c7435
rarayukawa <r1u2u18@gmail.com>
2021-08-17 22:14:40 +0900

    インスタンスフィールド

---
+++ sample/Vehicle.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Vehicle {
+	public String name;
+	Vehicle(String name) {
+		this.name = name;
+	}
+	public void run() {
+		System.out.println(this.name + "速い");
+		
+	}
+}
