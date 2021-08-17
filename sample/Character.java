66f3e7a636e721b1d3137445c30a7ff7abcefd1c
rarayukawa <r1u2u18@gmail.com>
2021-08-18 07:59:28 +0900

    練習問題

--- sample/Character.java
+++ sample/Character.java
@@ -1,5 +1,8 @@
 package sample;
 
 public class Character {
-
+	int hp;
+	String name;
+	public void attack(Matango m);
+	
 }
--- sample/Creature.java
+++ sample/Creature.java
@@ -1,5 +1,5 @@
 package sample;
 
 public interface Creature {
-
+	public abstract void run();
 }
