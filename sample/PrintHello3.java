fe3fbb0b77f43c70e5fe33cb2bb6c2b46a4ccbf9
rarayukawa <r1u2u18@gmail.com>
2021-08-29 15:37:42 +0900

    Thread

---
+++ sample/PrintHello3.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class PrintHello3 {
+	public static void main(String[] args) {
+		new LabelPrinter("おは！").start();
+		new LabelPrinter("こんちわ！").start();
+		new LabelPrinter("ばんわ！").start();
+		
+	}
+}
