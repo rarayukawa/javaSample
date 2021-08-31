3868278276faa09754badd24b06e9eb17b38dd6d
rarayukawa <r1u2u18@gmail.com>
2021-08-31 23:41:47 +0900

    正規表現

--- sample/StringTest2.java
+++ sample/StringTest2.java
@@ -7,5 +7,24 @@ public class StringTest2 {
 			sb.append("java");
 		}
 		String s = sb.toString();
+		String s2 = "Java";
+		System.out.println(s2.matches("Java"));
+		System.out.println(s2.matches("JavaJava"));
+		System.out.println(s2.matches("J.va"));
+		String s3 = "Jaaaaaaava";
+		System.out.println(s3.matches("Ja*va"));
+		String s4 = "dsjfdhdf hoerhoefewクォをw";
+		System.out.println(s4.matches(".*"));
+		
+		String s5 = "abc,def:ghi";
+		String[] words = s5.split("[,:]");
+		for(String w : words) {
+			System.out.print(w + "->");
+		}
 	}
+	
+	public boolean isValidPlayerName(String name) {
+		return name.matches("[A-Z][A-Z0-9]{7})");
+	}
+	
 }
