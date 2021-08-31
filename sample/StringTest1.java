ffadb2700d9ceca67da33d8864b1d96ba3c34263
rarayukawa <r1u2u18@gmail.com>
2021-08-31 23:20:59 +0900

    文字列処理

---
+++ sample/StringTest1.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class StringTest1 {
+	public static void main(String[] args) {
+		String s1 ="Java and JavaScript";
+		if (s1.contains("Java")) {
+			System.out.println("文字列s1は、Javawo含んでいます");
+		}
+		
+		if(s1.endsWith("Java")) {
+			System.out.println("文字列s1は、Javaが末尾にあります");
+		}
+		
+		System.out.println("文字列s1で最初にJavaが登場する位置は" + s1.indexOf("Java"));
+		System.out.println("文字列s1で最後にJavaが登場する位置は" + s1.lastIndexOf("Java"));
+	}
+}
