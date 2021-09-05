69d57adb82ea5acbc53df848dc1d438946ac1534
rarayukawa <r1u2u18@gmail.com>
2021-09-05 22:49:47 +0900

    Map要素の取り出し

--- sample/MapSample.java
+++ sample/MapSample.java
@@ -9,11 +9,18 @@ public class MapSample {
 		prefs.put("京都", 255);
 		prefs.put("東京", 1261);
 		prefs.put("熊本", 181);
+		prefs.put("三重", 11);
+		prefs.put("和歌山", 121);
+		prefs.put("大分", 341);
+		prefs.put("埼玉", 1321);
 		int tokyo = prefs.get("東京");
 		System.out.println("東京の人口は" + tokyo);
-		prefs.remove("京都");
 		prefs.put("熊本", 182);
 		int kumamoto = prefs.get("熊本");
 		System.out.println("熊本の人口は" + kumamoto);
+		for(String key : prefs.keySet()) {
+			int value = prefs.get(key);
+			System.out.println(key + "の人口は" + value);
+		}
 	}
 }
