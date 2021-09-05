9a117fc116fb87638f3c381ba8c529cd21ba286a
rarayukawa <r1u2u18@gmail.com>
2021-09-05 22:17:16 +0900

    コレクショn

---
+++ sample/Listsampl.java
@@ -0,0 +1,14 @@
+package sample;
+import java.util.ArrayList;
+
+public class Listsampl {
+	public static void main(String[] args) {
+		ArrayList<Integer> points = new ArrayList<Integer>();
+		points.add(10);
+		points.add(80);
+		points.add(75);
+		for(int i : points) {
+			System.out.println(i);
+		}
+	}
+}
