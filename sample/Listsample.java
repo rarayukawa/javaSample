ef2a0b910caae7d191be932fca25724872eaeb92
rarayukawa <r1u2u18@gmail.com>
2021-09-06 23:37:33 +0900

    ジェネリクス

--- sample/Listsample.java
+++ sample/Listsample.java
@@ -1,5 +1,22 @@
 package sample;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 public class Listsample {
-
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		
+		List<Integer> list2 = new ArrayList<Integer>();
+		
+		list2.add(1);
+		list.add("a");
+		list2.add(2);
+		
+		Iterator<Integer> iterator = list2.iterator();
+		while(iterator.hasNext()) {
+			int num = iterator.next();
+			System.out.println(num);
+		}
+	}
 }
