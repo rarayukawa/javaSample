d75e7e8ec46f72c762b350072befbe2a2aeecf4a
rarayukawa <r1u2u18@gmail.com>
2021-09-10 23:51:45 +0900

    Set

--- sample9/SetSample.java
+++ sample9/SetSample.java
@@ -1,5 +1,26 @@
 package sample9;
 
-public class SetSample {
+import java.util.HashSet;
 
+public class SetSample {
+	public static void main(String[] args) {
+		HashSet<String> set = new HashSet<>();
+		set.add("りんご");
+		set.add("みかん");
+		set.add("バナナ");
+		set.add("ぶどう");
+		set.remove("みかん");
+		System.out.println(set);
+		
+		if(set.contains("バナナ")) {
+			System.out.println("true");
+		} else {
+			System.out.println("fals");
+		}
+		if(set.contains("みかん")) {
+			System.out.println("true");
+		} else {
+			System.out.println("fals");
+		}
+	}
 }
