5275704ec3a655b044e38189dd79da12d4e5e5ad
rarayukawa <r1u2u18@gmail.com>
2021-09-11 00:02:46 +0900

    Map

--- sample9/MashSample.java
+++ sample9/MashSample.java
@@ -1,5 +1,16 @@
 package sample9;
 
-public class MashSample {
+import java.util.HashMap;
 
+public class MashSample {
+	public static void main(String[] args) {
+		HashMap<String, Integer> kamoku = new HashMap<>();
+		kamoku.put("国語", 90);
+		kamoku.put("数学", 80);
+		kamoku.put("英語", 70);
+		
+		for (String name : kamoku.keySet()) {
+			System.out.println(name + ":" + kamoku.get(name));
+		}
+	}
 }
